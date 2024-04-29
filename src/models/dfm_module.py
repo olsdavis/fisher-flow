Fix checkpoints and KL eval

src/models/dfm_module.py
@@ -130,9 +130,9 @@ class DNAModule(pl.LightningModule):
                 samples = self.dirichlet_flow_inference(x_0, None, self.net)[1]
                 acc += torch.nn.functional.one_hot(samples.argmax(dim=-1), self.net.dim).sum(dim=0)
                 to_draw -= n
-        acc /= self.kl_samples
+        acc /= acc.sum(dim=-1, keepdim=True)
         real_probs = self.trainer.test_dataloaders.dataset.probs.to(self.device)
-        kl = (acc * (acc.log() - real_probs.log())).sum().item()
+        kl = (acc * (acc.log() - real_probs.log())).sum(dim=-1).mean().item()
         self.log("test/kl", kl, on_step=False, on_epoch=True, prog_bar=False)
 
     def general_step(self, batch, batch_idx=None):

src/models/net/model.py
@@ -594,7 +594,8 @@ class CNNModel(nn.Module):
             self.cls_embedder = nn.Embedding(num_embeddings=self.num_cls + 1, embedding_dim=self.hidden)
             self.cls_layers = nn.ModuleList([Dense(self.hidden, self.hidden) for _ in range(self.num_layers)])
 
-    def forward(self, seq, t, cls = None, return_embedding=False):
+    def forward(self, x, t, cls = None, return_embedding=False):
+        seq = x
         if self.clean_data:
             feat = self.linear(seq)
             feat = feat.permute(0, 2, 1)

src/sfm/distribution.py
@@ -50,7 +50,7 @@ def estimate_categorical_kl(
     """
     assert sampling_mode in ["sample", "max"], "not a valid sampling mode"
     # init acc
-    acc = torch.zeros_like(real_dist, device=real_dist.device)
+    acc = torch.zeros_like(real_dist, device=real_dist.device).float()
 
     model.eval()
     to_sample = [batch] * (n // batch)
@@ -63,16 +63,20 @@ def estimate_categorical_kl(
         x_1 = manifold.tangent_euler(x_0, model, inference_steps)
         x_1 = manifold.send_to(x_1, NSimplex)
         if sampling_mode == "sample":
-            dist = Dirichlet(x_1)
-            samples = dist.sample()
-            acc += samples.sum(dim=0)
+            # TODO: remove or fix for Categorical
+            raise NotImplementedError("Sampling from Dirichlet not implemented")
+            # dist = Dirichlet(x_1)
+            # samples = dist.sample()
+            # acc += samples.sum(dim=0)
         else:
             samples = nn.functional.one_hot(
                 x_1.argmax(dim=-1),
                 real_dist.size(-1),
-            )
+            ).float()
             acc += samples.sum(dim=0)
 
-    acc /= float(n)
-    ret = (acc * (acc.log() - real_dist.log())).sum().item()
+    acc /= acc.sum(dim=-1, keepdim=True)
+    if not silent:
+        print(acc)
+    ret = (acc * (acc.log() - real_dist.log())).sum(dim=-1).mean().item()
     return ret