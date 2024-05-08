e1de37570b016df6acabdbf393d68acd8ed38236
olsdavis <olsdavis@gmail.com>
2024-05-08 15:14:18 +0100

    Add configs + scripts

---
+++ script/submit_unet_sfm_promoter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1   # nodes requested
+#SBATCH --job-name=sfm 
+#SBATCH --output=slurm/slurm_%j.out
+#SBATCH --error=slurm/slurm_%j.err
+#SBATCH --time=23:00:00
+#SBATCH --qos=medium
+#SBATCH --gres=gpu:4
+conda activate sfm
+python -m src.train experiment=promoter_sfm_unet1d trainer=ddp trainer.devices=4 data.batch_size=2048 logger=wandb
